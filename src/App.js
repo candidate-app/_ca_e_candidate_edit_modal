b6c3c6a70d12c03330e54e1beea0afca76ec178f
Jishnu <jishnu.nb@neoito.com>
2022-05-03 15:16:21 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import e_candidate_edit_modal from './e_candidate_edit_modal'
+export default function App() {
+  return (
+    <div>
+      <e_candidate_edit_modal/>
+    </div>
+  )
+}
---
+++ src/e_candidate_edit_modal.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const e_candidate_edit_modal = () => {
+  return (
+   <p>Hello from e_candidate_edit_modal </p>
+  )
+}
+export default e_candidate_edit_modal
